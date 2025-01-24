b23778174cd5b9081dbf6d7168e384e8736bf29b
Bibash45 <bibashcdry46@gmail.com>
2025-01-24 15:21:36 +0545

    topbar changes

--- frontend/src/components/userComponents/Topbar.jsx
+++ frontend/src/components/userComponents/Topbar.jsx
@@ -2,35 +2,35 @@ import React from "react";
 
 const Topbar = () => {
   return (
-    <div className=" h-7 bg-white text-end z-10 flex justify-center md:justify-between items-center  md:pr-6 lg:pr-10">
+    <div className="h-7 bg-white text-end z-10 ">
       {/* <!-- Marquee Section --> */}
-      <div className="hidden md:block w-full md:w-[50%] lg:w-[70%] overflow-hidden">
-        <div className="scrolling-text">
-          <marquee className="text-amber-500 text-sm md:text-base font-semibold ">
-            SALE - UPTO 80% OFF + EXTRA 10% OFF ON PREPAID ORDERS &nbsp; &nbsp;
-            &nbsp; &nbsp; &nbsp; SALE - UPTO 80% OFF + EXTRA 10% OFF ON PREPAID
-            ORDERS &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; SALE - UPTO 80% OFF +
-            EXTRA 10% OFF ON PREPAID ORDERS &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-            SALE - UPTO 80% OFF + EXTRA 10% OFF ON PREPAID ORDERS &nbsp; &nbsp;
-            &nbsp; &nbsp; &nbsp; SALE - UPTO 80% OFF + EXTRA 10% OFF ON PREPAID
-            ORDERS &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; SALE - UPTO 80% OFF +
-            EXTRA 10% OFF ON PREPAID ORDERS &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-            SALE - UPTO 80% OFF + EXTRA 10% OFF ON PREPAID ORDERS
-          </marquee>
-        </div>
-      </div>
-
-      {/* <!-- Navigation Links Section --> */}
-      <div className="flex justify-end gap-4 md:gap-6 items-center text-sm md:text-sm lg:text-md">
-        <p className="tracking-tighter text-[#735DA5] cursor-pointer hover:text-[#9b75c6] transition-colors">
-          SAVE MORE ON APP
-        </p>
-        <p className="tracking-tighter text-[#735DA5] cursor-pointer hover:text-[#9b75c6] transition-colors">
-          BECOME A SELLER
-        </p>
-        <p className="tracking-tighter text-[#735DA5] cursor-pointer hover:text-[#9b75c6] transition-colors">
-          HELP AND SUPPORT
-        </p>
+      <div className=" overflow-hidden ">
+        <marquee className="text-amber-500 text-sm md:text-base font-semibold">
+          SALE - UPTO<span className="text-[#735DA5]"> 80% OFF </span> + EXTRA{" "}
+          <span className="text-[#735DA5]"> 10% OFF </span> ON PREPAID ORDERS
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+          SALE - UPTO<span className="text-[#735DA5]"> 80% OFF </span> + EXTRA{" "}
+          <span className="text-[#735DA5]"> 10% OFF </span> ON PREPAID ORDERS
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+          SALE - UPTO<span className="text-[#735DA5]"> 80% OFF </span> + EXTRA{" "}
+          <span className="text-[#735DA5]"> 10% OFF </span> ON PREPAID ORDERS
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+          SALE - UPTO<span className="text-[#735DA5]"> 80% OFF </span> + EXTRA{" "}
+          <span className="text-[#735DA5]"> 10% OFF </span> ON PREPAID ORDERS
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+          SALE - UPTO<span className="text-[#735DA5]"> 80% OFF </span> + EXTRA{" "}
+          <span className="text-[#735DA5]"> 10% OFF </span> ON PREPAID ORDERS
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+          SALE - UPTO<span className="text-[#735DA5]"> 80% OFF </span> + EXTRA{" "}
+          <span className="text-[#735DA5]"> 10% OFF </span> ON PREPAID ORDERS
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+          SALE - UPTO<span className="text-[#735DA5]"> 80% OFF </span> + EXTRA{" "}
+          <span className="text-[#735DA5]"> 10% OFF </span> ON PREPAID ORDERS
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+          SALE - UPTO<span className="text-[#735DA5]"> 80% OFF </span> + EXTRA{" "}
+          <span className="text-[#735DA5]"> 10% OFF </span> ON PREPAID ORDERS
+          &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        </marquee>
       </div>
     </div>
   );
